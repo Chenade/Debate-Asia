3fc9450b6333bbc6d89783ef88fdeac485a0635a
湯雅惠 <yahuitang227@gmail.com>
2023-02-27 19:56:13 +0100

    rearrange grid - neg

--- develop/resources/views/page/grading.blade.php
+++ develop/resources/views/page/grading.blade.php
@@ -79,7 +79,7 @@
                         </div>
                         <div class="col-6 row">
                             <div><p>{{trans('rules.sum_score')}}</p></div>
-                            <div class="col-12 d-flex">
+                            <div class="col-9 d-flex">
                                 <div class="col-8" style="padding: 0"><input type="number" min="0" max="500" class="pos_score col-12 inputbox" data-id="5" disabled></div>
                                 <div class="col-4 d-flex align-items-center" style="padding: 0">/500</div>
                             </div>
@@ -207,14 +207,25 @@
                     </div>
                 </div>
                 <div class="col-6" >
-                    <p>{{trans('dictionary.neg')}}{{trans('dictionary.grade')}}：</p>
+                    <div class="col-12 row">
+                        <div class="col-6">
+                            <p>{{trans('dictionary.neg')}}{{trans('dictionary.grade')}}：</p>
+                        </div>
+                        <div class="col-6 row">
+                            <div><p>{{trans('rules.sum_score')}}</p></div>
+                            <div class="col-12 d-flex">
+                                <div class="col-8" style="padding: 0"><input type="number" min="0" max="500" class="neg_score col-12 inputbox" data-id="5" disabled></div>
+                                <div class="col-4 d-flex align-items-center" style="padding: 0">/500</div>
+                            </div>
+                        </div>
+                    </div>
+                    
                     <table class="col-12">
                         <tr>
                             <td class="col-2">{{trans('rules.score_1_title')}}</td>
                             <td class="col-2">{{trans('rules.score_2_title')}}</td>
                             <td class="col-2">{{trans('rules.score_3_title')}}</td>
                             <td class="col-2">{{trans('rules.score_4_title')}}</td>
-                            <td class="col-2">{{trans('rules.sum_score')}}</td>
                         </tr>
                         <tr>
                             <td class="col-2">
@@ -239,12 +250,6 @@
                                 <div class="col-12 d-flex">
                                     <div class="col-8" style="padding: 0"><input type="number" min="0" max="50" class="neg_score col-12 inputbox" data-id="4"></div>
                                     <div class="col-4 d-flex align-items-center" style="padding: 0">/50</div>
-                                </div>
-                            </td>
-                            <td class="col-2">
-                                <div class="col-12 d-flex">
-                                    <div class="col-8" style="padding: 0"><input type="number" min="0" max="500" class="neg_score col-12 inputbox" data-id="5" disabled></div>
-                                    <div class="col-4 d-flex align-items-center" style="padding: 0">/500</div>
                                 </div>
                             </td>
                         </tr>
