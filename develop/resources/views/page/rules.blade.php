a4497448091b4c96e095effeff1372b7a7662886
YANGCHI KUO <chenade0312@gmail.com>
2025-03-08 19:54:37 +0100

    update rules to pdf file

--- develop/resources/views/page/rules.blade.php
+++ develop/resources/views/page/rules.blade.php
@@ -13,48 +13,12 @@
                 </div>
             </div>
 
-            <div class="d-flex flex-wrap align-items-center" style="padding: 0 90px 0 110px; margin-bottom: 50px;">
-
-                <p style="text-indent: -2em; color: black; font-weight:500; text-align:justify;">{!! trans('rules.r1') !!}</p>
-
-                <p style="text-indent: -2em; color: black; font-weight:500; text-align:justify;">{!! trans('rules.r2') !!}</p>
-
-                <p style="text-indent: -2em; color: black; font-weight:500; text-align:justify;"> {!! trans('rules.r3') !!}</p>
-
-                <p style="text-indent: -2em; color: black; font-weight:500; text-align:justify;"> {!! trans('rules.r4') !!}</p>
-
-                <p style="text-indent: -2em; color: black; font-weight:500; text-align:justify;"> {!! trans('rules.r5') !!}</p>
-
-                <p style="text-indent: -2em; color: black; font-weight:500; text-align:justify;"> {!! trans('rules.r6') !!}</p>
-                <p style="text-indent: -2em; color: black; font-weight:500; text-align:justify;"> {!! trans('rules.r7') !!}</p>
-                <p style="text-indent: -2em; color: black; font-weight:500; text-align:justify;"> {!! trans('rules.r8') !!}</p>
-
-                <p style="text-indent: -2em; color: black; font-weight:500; text-align:justify; margin :0;"> {!! trans('rules.r9') !!} <br> 
-                {!! trans('rules.r90') !!} </p>
-                    <p style="color: rgba(150, 0, 0, 1);"> 
-                        {!! trans('rules.r91') !!}<br> 
-                        {!! trans('rules.r92') !!}<br> 
-                        {!! trans('rules.r93') !!}
-                    </p>
-                    <p style="color: rgba(150, 0, 0, 1);"> 
-                        {!! trans('rules.r94') !!}<br> 
-                        {!! trans('rules.r95') !!}<br> 
-                        {!! trans('rules.r96') !!}
-                    </p>
-
-                <p style="text-indent: -2em; color: black; font-weight:500; text-align:justify;"> {!! trans('rules.r10') !!}<br>
-                    {!! trans('rules.r101') !!}
-                    <br>
-                    {!! trans('rules.r102') !!}<br>
-                    {!! trans('rules.r103') !!}<br>
-                    {!! trans('rules.r104') !!}<br>
-
-                <p>
-                    {!! trans('rules.ps1') !!}<br>
-                    {!! trans('rules.ps2') !!}
-                </p>
-
+            <div class="col-12 mb-5">
+                <div class="pdf-viewer">
+                    <iframe src="/pdf/rules_2025.pdf" width="100%" height="800px"></iframe>
+                </div>
             </div>
+
         </div>
     </div>
 
